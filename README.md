17e8811460f0c5b99b7e115ff0e64a26dda4a748
Thomas Ikimi <themerc01@gmail.com>
2017-08-29 17:07:08 +0100

    1-5 challenges

---
+++ README.md
@@ -0,0 +1,2 @@
+**BORIS BIKES CHALLENGES**
+Working through the 25 challenges of Boris Bikes with pair partner
---
+++ spec/docking_station_spec.rb
@@ -0,0 +1,5 @@
+describe 'dockingstation' do
+  it 'returns "correct" when passed X' do
+    expect(DockingStation.new).to eq 'dockingstation'
+  end
+end
