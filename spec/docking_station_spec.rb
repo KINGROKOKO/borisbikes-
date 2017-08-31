0e131451bc30f661bdd26c1da89d643ee7c26f5e
Thomas Ikimi <themerc01@gmail.com>
2017-08-31 16:43:39 +0100

    11

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -7,4 +7,16 @@ describe DockingStation do
     bike = subject.release_bike
     expect(bike).to be_working
   end
+
+  it 'docks bike' do
+    bike=Bike.new
+    expect(subject.dock(bike)).to eq bike
+  end
+
+  it 'bike available' do
+    bike=Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq bike
+  end
+
 end
